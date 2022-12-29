c06e8ec847ca557d65fc2d2ccfe6f50d287d8af1
ivanbiljan <ssbiljan@gmail.com>
2022-12-29 17:59:54 +0100

    Add MustHaveTenantInterceptor

---
+++ backend/NaprednioJsProgramiranje/Njs.Core/Constants.cs
@@ -0,0 +1,37 @@
+﻿namespace Njs.Core;
+
+public static class Constants
+{
+    public const string InvalidTenantId = nameof(InvalidTenantId);
+
+    public static class Claims
+    {
+        public const string Sid = nameof(Sid);
+
+        public const string TenantId = nameof(TenantId);
+    }
+
+    public static class VerificationLinks
+    {
+        public const int AccountConfirmationExpirationMinutes = 15;
+        
+        public static string GetAccountConfirmationLink(string token) => $"/account/confirm/{token}";
+
+        public static string GetPasswordResetLink(string token) => $"/account/reset-password/{token}";
+    }
+
+    public static class Jwt
+    {
+        public const int AccessTokenExpirationDays = 1;
+
+        public const int RefreshTokenExpirationDays = 30;
+    }
+
+    public static class Hangfire
+    {
+        public static class TenantParameters
+        {
+            public const string TenantId = nameof(TenantId);
+        }
+    }
+}
---
+++ backend/NaprednioJsProgramiranje/Njs.Core/Infrastructure/Persistence/Interceptors/MustHaveTenantInterceptor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.VisualBasic;
+using Njs.Core.Infrastructure.Data;
+using Njs.Core.Shared.Multitenancy;
+
+namespace Njs.Core.Infrastructure.Persistence.Interceptors;
+
+internal sealed class MustHaveTenantInterceptor : SaveChangesInterceptor
+{
+    private readonly TenantProviderResult _tenantProviderResult;
+
+    public MustHaveTenantInterceptor(ITenantResolver tenantResolver)
+    {
+        _tenantProviderResult = tenantResolver.GetTenant();
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = new())
+    {
+        foreach (var entry in eventData.Context.ChangeTracker.Entries())
+        {
+            if (entry.Entity is not IMustHaveTenant { TenantId: null } mustHaveTenant ||
+                entry.State != EntityState.Added)
+            {
+                continue;
+            }
+
+            var tenantId = _tenantProviderResult.TenantId!;
+
+            if (tenantId == Constants.InvalidTenantId)
+            {
+                entry.State = EntityState.Detached;
+            }
+
+            mustHaveTenant.TenantId = tenantId;
+        }
+
+        return ValueTask.FromResult(result);
+    }
+}
