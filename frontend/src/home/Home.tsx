fc2e087e9a3ce7dd8ff2d18c78e734331a45a60b
ivanbiljan <ssbiljan@gmail.com>
2022-12-31 16:00:40 +0100

    Add Header and Footer to home page

--- frontend/src/home/Home.tsx
+++ frontend/src/home/Home.tsx
@@ -1,12 +1,16 @@
 import { YayCard } from "./../shared/yay-card/YayCard";
 import Loader from "/src/assets/svgs/yay-logo.svg";
 import { YayCarousel } from "./../shared/yay-carousel/YayCarousel";
+import Header from "../layout/Header";
+import Footer from "../layout/Footer";
 
 export const Route = "/";
 
 export const Home = () => {
     return (
         <>
+            <Header/>
+            <Footer/>
         </>
     );
 };
