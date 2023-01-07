bb185a710bd27d272c70941ba531c0d52139cd8e
ivanbiljan <ssbiljan@gmail.com>
2023-01-07 19:58:35 +0100

    Move ProductsController to the appropriate file

---
+++ backend/NaprednioJsProgramiranje/Njs/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Njs.Core.Features.Products;
+
+namespace Njs.Controllers;
+
+[ApiController]
+[Route("api/products")]
+[AllowAnonymous]
+public sealed class ProductsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ProductsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpGet]
+    public async Task<GetAllProductsResponse> GetAll([FromQuery] GetAllProductsRequest request)
+    {
+        return await _mediator.Send(request);
+    }
+}
